cc54ed77d9c7a317737899ecb03d2a92d471283b
techpappu <psaiduzzaman@gmail.com>
2026-03-27 15:16:39 +0600

    small eid change in checkout field

--- functions.php
+++ functions.php
@@ -229,8 +229,8 @@ add_filter('woocommerce_checkout_fields', function ($fields) {
 
 	/* Address field */
 	$fields['billing']['billing_address_1']['label']       = 'ঠিকানা';
-	//$fields['billing']['billing_address_1']['placeholder'] = 'থানা: রামপুর, জেলা: ঢাকা';
-	$fields['billing']['billing_address_1']['placeholder'] = 'ঈদের ৬–৭ দিনের পর ডেলিভারি করা হবে। সঠিক ঠিকানা লিখুন।';
+	$fields['billing']['billing_address_1']['placeholder'] = 'থানা: রামপুর, জেলা: ঢাকা';
+	//$fields['billing']['billing_address_1']['placeholder'] = 'ঈদের ৬–৭ দিনের পর ডেলিভারি করা হবে। সঠিক ঠিকানা লিখুন।';
 	$fields['billing']['billing_address_1']['priority']    = 30;
 
 	return $fields;
